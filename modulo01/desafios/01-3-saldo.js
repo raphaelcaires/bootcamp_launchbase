761fa49de1ffa2bbbbf697dfd9a40b91372c6bb9
raphaelcaires <raphaelcaires@gmail.com>
2021-09-25 21:09:41 -0300

    modulo_01

---
+++ modulo01/desafios/01-3-saldo.js
@@ -0,0 +1,27 @@
+// Calcular saldo dos usuários
+const usuariosDoBanco = [
+  {
+    nome: "Raphael",
+    receitas: [115.3, 48.7, 98.3, 14.5],
+    despesas: [85.3, 13.5, 19.9]
+  },
+  {
+    nome: "Jonathas",
+    receitas: [24.6, 214.3, 45.3],
+    despesas: [185.3, 12.1, 120.0]
+  },
+  {
+    nome: "Mariana",
+    receitas: [9.8, 120.3, 340.2, 45.3],
+    despesas: [450.2, 29.9]
+  },
+];
+
+function calculaSaldos(receitas, despesas) {
+
+}
+
+function somaNumeros(numeros) {
+  numeros = 0;
+  for (let i = 0; i < )
+}
